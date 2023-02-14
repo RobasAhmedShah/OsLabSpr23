a0ebdfd7f09012e594254decf46cac9ef2626899
RobasAhmedShah <123714538+RobasAhmedShah@users.noreply.github.com>
2023-02-14 22:16:21 +0500

    Add files via upload

---
+++ Lab 02/Q4/shell4.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+touch filename.txt
+filepath="/home/kali/Desktop/OSLAB02/test01/01/test.txt"
+filename=$(basename "$filepath")
+echo "Full Name : $filename" >> filename.txt 
+
+
+
+filepath="/home/kali/Desktop/OSLAB02/test01/02/test.txt"
+filename=$(basename "$filepath")
+echo -e "\nFull Name : $filename"  >> filename.txt
+
+
+filepath="/home/kali/Desktop/OSLAB02/test02/01/test.txt"
+filename=$(basename "$filepath")
+echo -e "\nFull Name : $filename" >> filename.txt
+
+
+filepath="/home/kali/Desktop/OSLAB02/test02/02/test.txt"
+filename=$(basename "$filepath")
+echo -e "\nFull Name : $filename" >> filename.txt
+
+
+filepath="/home/kali/Desktop/OSLAB02/test03/01/test.txt"
+filename=$(basename "$filepath")
+echo -e "\nFull Name : $filename" >> filename.txt
+
+
+filepath="/home/kali/Desktop/OSLAB02/test03/02/test.txt"
+filename=$(basename "$filepath")
+echo -e "\nFull Name : $filename" >> filename.txt
+
+
+filepath="/home/kali/Desktop/OSLAB02/test04/01/test.txt"
+filename=$(basename "$filepath")
+echo -e "\nFull Name : $filename" >> filename.txt
+
+
+filepath="/home/kali/Desktop/OSLAB02/test04/02/test.txt"
+filename=$(basename "$filepath")
+echo -e "\nFull Name : $filename" >> filename.txt
+
+
+filepath="/home/kali/Desktop/OSLAB02/test05/01/test.txt"
+filename=$(basename "$filepath")
+echo -e "\nFull Name : $filename" >> filename.txt
+
+
+filepath="/home/kali/Desktop/OSLAB02/test05/02/test.txt"
+filename=$(basename "$filepath")
+echo -e "\nFull Name : $filename" >> filename.txt
+
+
+
